5a23c1f35664e26f6493f70179674ee6c591f61e
LucasBbe <lucas.boube@gmail.com>
2024-10-30 17:09:31 +0100

    React Fetcher7

---
+++ src/components/EmployeeCard.tsx
@@ -0,0 +1,30 @@
+import "../style/EmployeeCard.css";
+
+interface EmployeeCardProps {
+  employee: {
+    name: {
+      first: string;
+      last: string;
+    };
+    email: string;
+    picture: {
+      medium: string;
+    };
+  };
+}
+
+function EmployeeCard({ employee }: EmployeeCardProps) {
+  return (
+    <figure className="DisplayEmployee">
+      <img src={employee.picture.medium} alt={employee.name.first} />
+      <figcaption>
+        <strong>
+          {employee.name.first} {employee.name.last}
+        </strong>
+      </figcaption>
+      <p>{employee.email}</p>
+    </figure>
+  );
+}
+
+export default EmployeeCard;
