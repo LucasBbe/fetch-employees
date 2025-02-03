58c981d170e2ea5922d035a0d768cb9d72b1c84f
LucasBbe <lucas.boube@gmail.com>
2025-02-03 09:52:20 +0100

    Express back-end and link between front-end and back-end

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,35 @@ import { useState } from "react";
 import EmployeeCard from "./components/EmployeeCard";
 
 const sampleEmployee = {
-  name: {
-    first: "Charlie",
-    last: "Thompson",
-  },
-  email: "charlie.thompson@example.com",
-  picture: {
-    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
-  },
+	name: {
+		first: "Charlie",
+		last: "Thompson",
+	},
+	email: "charlie.thompson@example.com",
+	picture: {
+		medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+	},
 };
 
 function App() {
-  const [employee, setEmployee] = useState(sampleEmployee);
-  const getEmployee = () => {
-    // Send the request
-    fetch("https://randomuser.me/api?nat=en")
-      .then((response) => response.json())
-      .then((data) => {
-        setEmployee(data.results[0]);
-      });
-  };
+	const [employee, setEmployee] = useState(sampleEmployee);
+	const getEmployee = () => {
+		// Send the request
+		fetch("http://localhost:3310/api/employees")
+			.then((response) => response.json())
+			.then((data) => {
+				setEmployee(data.results[0]);
+			});
+	};
 
-  return (
-    <div className="App">
-      <EmployeeCard employee={employee} />
-      <button type="button" onClick={getEmployee}>
-        Get employee
-      </button>
-    </div>
-  );
+	return (
+		<div className="App">
+			<EmployeeCard employee={employee} />
+			<button type="button" onClick={getEmployee}>
+				Get employee
+			</button>
+		</div>
+	);
 }
 
 export default App;
